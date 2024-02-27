voxpupuli/puppet-aptly

commit 40bdf883dbd78ce8976339e08e4473a5213f6d64

repo: parameters: default to undef

manifests/repo.pp
@@ -22,11 +22,11 @@
 
 define aptly::repo(
   Array $architectures = [],
-  String $comment       = '',
-  String $component     = '',
-  String $distribution  = '',
-){
-  include ::aptly
+  Optional[String[1]] $comment       = undef,
+  Optional[String[1]] $component     = undef,
+  Optional[String[1]] $distribution  = undef,
+) {
+  include aptly
 
   $aptly_cmd = "${::aptly::aptly_cmd} repo"
 
@@ -37,22 +37,22 @@ define aptly::repo(
     $architectures_arg = "-architectures=\"${architectures_as_s}\""
   }
 
-  if empty($comment) {
+  $comment_arg = if $comment {
+    "-comment=\"${comment}\""
+  } else {
     $comment_arg = ''
-  } else{
-    $comment_arg = "-comment=\"${comment}\""
   }
 
-  if empty($component) {
-    $component_arg = ''
-  } else{
-    $component_arg = "-component=\"${component}\""
+  $component_arg = if $component {
+    "-component=\"${component}\""
+  } else {
+    ''
   }
 
-  if empty($distribution) {
-    $distribution_arg = ''
-  } else{
-    $distribution_arg = "-distribution=\"${distribution}\""
+  $distribution_arg = if $distribution {
+    "-distribution=\"${distribution}\""
+  } else {
+    ''
   }
 
   exec{ "aptly_repo_create-${title}":

commit 975cac3d6f6f84ade8d24eb30ac0a60f4ea14f3b

repo.pp: fix duplicate variable assignment
This was broken in https://github.com/voxpupuli/puppet-aptly/commit/40bdf883dbd78ce8976339e08e4473a5213f6d64

manifests/repo.pp
@@ -40,7 +40,7 @@ define aptly::repo(
   $comment_arg = if $comment {
     "-comment=\"${comment}\""
   } else {
-    $comment_arg = ''
+    ''
   }
 
   $component_arg = if $component {