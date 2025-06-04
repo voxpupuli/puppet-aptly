e1a2dccb061378684c85f118abc59cbd97de8cd4
Yury Bushmelev <jay4mail@gmail.com>
2025-06-04 12:13:23 +0800

    refactor: Make the CLI helper a Puppet eXtension
    No functional changes, just reshuffling files & renaming classes.

--- tasks/publish_list.rb
+++ tasks/publish_list.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative '../lib/cli_helper'
+require_relative '../lib/puppet_x/voxpupuli/aptly/cli_helper'
 require_relative '../../ruby_task_helper/files/task_helper' unless Object.const_defined?('TaskHelper')
 
 # List publishs
 class AptlyPublishListTask < TaskHelper
   def task(opts = {})
-    @cli_helper ||= opts.delete(:cli_helper) || Aptly::CliHelper.new(opts)
+    @cli_helper ||= opts.delete(:cli_helper) || PuppetX::Aptly::CliHelper.new(opts)
     begin
       { publish_list: @cli_helper.publish_list(opts) }
     rescue Aptly::Error => e
